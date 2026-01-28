f8e2e9fcedbcca1ba33ffaeb098ed32e1d1e828b
Marlon Frazão <frazao.marlon@gmail.com>
2026-01-27 21:02:09 -0300

    consuming exchange-service

--- src/main/java/com/marlon/controller/BookController.java
+++ src/main/java/com/marlon/controller/BookController.java
@@ -1,5 +1,6 @@
 package com.marlon.controller;
 
+import com.marlon.dto.Exchange;
 import com.marlon.environment.InstanceInformationService;
 import com.marlon.model.Book;
 import com.marlon.repository.BookRepository;
@@ -9,8 +10,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
+import java.math.BigDecimal;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 
 @RestController
 @RequestMapping("book-service")
@@ -31,9 +37,22 @@ public class BookController {
         String port = informationService.retrieveServerPort();
 
         var book = repository.findById(id).orElseThrow(() -> new RuntimeException("Book not found."));
+
+        Map<String, String> params = new HashMap<>();
+        params.put("amount", book.getPrice().toString());
+        params.put("from", "USD");
+        params.put("to", currency);
+
+        var response = new RestTemplate()
+               .getForEntity("http://localhost:8000/exchange-service/" +
+                       "{amount}/{from}/{to}", Exchange.class, params);
+
+        Exchange exchange = response.getBody();
+
         book.setEnvironment(port);
+        book.setPrice(Objects.requireNonNullElse(exchange, new Exchange()).getConvertedValue());
         book.setCurrency(currency);
-        
+
         return book;
     }
 }
